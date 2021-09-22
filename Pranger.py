6614fb87073ff44985fcc4c7a487047d498b3efe
twothreenine <leonard_ostler@yahoo.de>
2021-09-22 12:20:18 +0200

    Add base methods, hook up Pranger webscraping with base

--- Pranger.py
+++ Pranger.py
@@ -1,37 +1,154 @@
 from bs4 import BeautifulSoup
 import requests
+import base
+import re
 
-def getCategory(id) :
-  response = requests.get("https://oekobox-online.eu/v3/shop/pranger/s2/C6.0.219C/category.jsp?categoryid="+str(id)+"&cartredir=1")
+supplier = "Biohof Pranger"
+categories_to_ignore = []
+subcategories_to_ignore = []
+articles_to_ignore = []
 
-  html_string = response.text
-  parsed_html = BeautifulSoup(html_string,features="html.parser")
+categories = []
+articles = []
+ignored_categories = []
+ignored_subcategories = []
+ignored_articles = []
+message = []
 
-  table = parsed_html.body.find_all('table')
-  name  = parsed_html.body.find(class_="font2 ic2")
+menu = BeautifulSoup(requests.get("https://oekobox-online.eu/v3/shop/pranger/s2/C6.0.222C/categories.jsp?intern=1").text, features="html.parser")
 
-  return {"name" : name, "items" : table}
+class PriceOption:
+  def __init__(self, price, unit):
+    self.price = price
+    self.unit = unit
 
-def printIfFound(src, class_name, prefix) :
+def getSubcategories(id):
+  subcats = []
+  print('sg'+str(id))
+  category = base.Category(number=id)
+  subgroup = menu.body.find(id=('sg'+str(id)))
+  if subgroup:
+    subcat_links = subgroup.find_all('a')
+    for sc in subcat_links:
+      number = sc['href'].split("id=")[-1]
+      name = sc.get_text().strip()
+      subcategory = base.Category(number=number, name=name)
+      if number in subcategories_to_ignore:
+        ignored_subcategories.append(subcategory)
+      else:
+        category.subcategories.append(subcategory)
+        parsed_html = BeautifulSoup(requests.get("https://oekobox-online.eu/v3/shop/pranger/s2/C6.0.219C/"+sc['href']).text, features="html.parser")
+        table = parsed_html.body.find_all('table')
+        subcats.append({"name" : name, "items" : table})
+
+  if category.number in categories_to_ignore:
+    ignored_categories.append(category)
+  else:
+    categories.append(category)
+    return subcats
+
+def getCategory(id):
+  parsed_html = BeautifulSoup(requests.get("https://oekobox-online.eu/v3/shop/pranger/s2/C6.0.219C/category.jsp?categoryid="+str(id)+"&cartredir=1").text, features="html.parser")
+  name  = parsed_html.body.find(class_="font2 ic2").get_text().strip()
+  existing_categories = [x for x in categories if x.number == id]
+  if existing_categories:
+    category = existing_categories[0]
+    category.name = name
+  else:
+    category = base.Category(number=id, name=name)
+  if id in categories_to_ignore:
+    ignored_categories.append(category)
+  else:
+    if not existing_categories:
+      categories.append(category)
+    table = parsed_html.body.find_all('table')
+    return [{"name" : name, "items" : table}]
+
+def getIfFound(src, class_name):
   item = src.find(class_=class_name)
+  text = ""
   if item and item.get_text().strip() != "" : 
-    print(prefix + ": " + item.get_text().strip())
+    text = item.get_text().strip()
+  return text
+
+def getArticles(category):
+  for subcat in category:
+    if subcat["items"] == [] : 
+      continue
+    cat_name = subcat["name"]
+    print(cat_name)
+
+    for item in subcat["items"] :
+      item_link = item.find(class_="font2 ic3 itemname")['href']
+      order_number = item_link.split("id=")[-1]
+      if [x for x in articles if x.order_number == order_number]:
+        continue
+      title = item.find(class_="font2 ic3 itemname").text.replace("Bio-", "").replace(" Pkg.", "").replace(" PKG.", "").replace(" Pkg", "").replace(" PKG", "").replace(" Stk.", "").replace(" Bd.", "").replace(" Str.", "").replace(" kg", "").strip()
+      title_contents = re.split("(.+)\s(\d.?\d*.?\S+)\s?([a-zA-Z]*)", title)
+      if len(title_contents) > 1:
+        name = title_contents[1]
+        if title_contents[3]:
+          name += " " + title_contents[3]
+      else:
+        name = title
+      producer = getIfFound(item, "ic2 producer")
+      note = getIfFound(item, "ic2 cinfotxt").replace("/Pkg.", "").replace(" Inhaltfüllung", "")
+      origin = ""
+      if producer == "Landwirtschaft Pranger" or producer == "Produktion Biohof A. Pranger e.U.":
+        origin = "eigen"
+      else:
+        item_details = BeautifulSoup(requests.get("https://oekobox-online.eu/v3/shop/pranger/s2/C6.0.222C/"+item_link).text,features="html.parser").body
+        address = getIfFound(item_details, "oo-producer-address")
+        if address:
+          origin = address.replace("Österreich", "").replace("AT-", "").replace("A-", "").strip()
+        else:
+          origin = getIfFound(item, "herkunft")
+
+      price_options = item.find(class_="font2 ic2 baseprice")
+      prices = []
+      for option in price_options.find_all("option") + price_options.find_all(class_="oo-item-price") :
+        price, unit = option.get_text().strip().replace("ca. ", "").split("€/")
+        new_option = PriceOption(float(price), unit)
+        prices.append(new_option)
+
+      unit_info = ""
+      if len(prices) == 1:
+        if len(title_contents) > 1:
+          unit_info = title_contents[2]
+        elif note:
+          unit_in_note = re.split("([c]?[a]?[.]?[ ]?\d+[.,]?\d*[a-zA-Z]+)", note)
+          if len(unit_in_note) > 1:
+            unit_info = unit_in_note[1]
+            note = note.replace(unit_in_note[1], "").strip()
+        if unit_info:
+          prices[0].unit = unit_info + " " + prices[0].unit
+
+      if len(unit) > 15:
+        unit = unit.replace("Flasche", "Fl.").replace("Packung", "Pkg.").replace("Stück", "Stk.")
+
+      prices.sort(key=lambda x: x.price)
+      favorite_option = None
+      for option in prices:
+        if option.price >= 1:
+          favorite_option = option
+          break
+      if not favorite_option:
+        favorite_option = prices[-1]
+
+      article = base.Article(order_number=order_number, name=name, note=note, unit=favorite_option.unit, price_net=favorite_option.price, category=cat_name, manufacturer=producer, origin=origin, orig_name=name, orig_unit=unit_info)
+      if article.order_number in articles_to_ignore:
+        ignored_articles.append(article)
+      else:
+        articles.append(article)
+
+for idx in range(20) :
+  cat = getSubcategories(idx)
+  getArticles(cat)
 
 for idx in range(20) :
   cat = getCategory(idx)
-  if cat["items"] == [] : 
-    continue
+  getArticles(cat)
 
-  print(cat["name"].get_text().strip())
-
-  for item in cat["items"] :
-    print("  " + item.find(class_="font2 ic3 itemname").text)
-    printIfFound(item, "ic2 producer", "    Hersteller")
-    printIfFound(item, "ic2 cinfotxt", "    Info")
-    print("    Kaufoptionen:")
-    price = item.find(class_="font2 ic2 baseprice")
-    
-    for option in price.find_all("option") + price.find_all(class_="oo-item-price") :
-      print("      " + option.get_text().strip())
-
-  idx = idx+1
+articles = base.rename_duplicates(articles)
+base.send_message(supplier, categories, ignored_categories, ignored_subcategories, ignored_articles)
+base.write_csv(supplier=supplier, articles=articles)
--- base.py
+++ base.py
@@ -3,7 +3,7 @@ import os
 import datetime
 
 class Article:
-    def __init__(self, order_number, name, unit, price_net, available=True, note="", manufacturer="", origin="", vat=0, deposit=0, unit_quantity=1, category=""):
+    def __init__(self, order_number, name, unit, price_net, available=True, note="", manufacturer="", origin="", vat=0, deposit=0, unit_quantity=1, category="", orig_name="", orig_unit="", temp=""):
         self.available = available
         self.order_number = order_number
         self.name = name
@@ -16,6 +16,34 @@ class Article:
         self.deposit = deposit
         self.unit_quantity = unit_quantity
         self.category = category
+        self.orig_name = orig_name # for finding duplicates, not loaded into foodsoft
+        self.orig_unit = orig_unit # for distinguishing duplicates, not loaded into foodsoft
+        self.temp = temp # for temporary data of the script, not loaded into foodsoft
+
+class Category:
+    def __init__(self, number, name="", subcategories=[]):
+        self.number = number
+        self.name = name
+        self.subcategories = subcategories
+
+def suffix(suffix):
+    return " (" + str(suffix) + ")"
+
+def rename_duplicates(articles):
+    for article in articles:
+        articles_of_this_name = [a for a in articles if a.orig_name == article.orig_name]
+        if len(articles_of_this_name) > 1:
+            if article.orig_unit:
+                articles_of_this_unit = [a for a in articles_of_this_name if a.orig_unit == article.orig_unit]
+                if len(articles_of_this_unit) != len(articles_of_this_name):
+                    article.name += suffix(article.orig_unit)
+
+    for article in articles:
+        articles_of_this_name = [a for a in articles if a.name == article.name]
+        if len(articles_of_this_name) > 1:
+            article.name += suffix(articles_of_this_name.index(article) + 1)
+
+    return articles
 
 def validate_string(string, string_type):
     string = str(string)
@@ -43,6 +71,42 @@ def get_data_from_articles(articles):
         rows.append(article_data)
 
     return rows
+
+def listCategories(categories):
+    text = ""
+    for category in categories:
+        text += "#" + str(category.number) + " " + category.name
+        if category.subcategories:
+            subcats = category.subcategories.copy()
+            text += " (inkl. Unterkategorien " + subcats[0].name
+            subcats.pop(0)
+            for sc in subcats:
+                text += ", " + sc.name
+            text += ")"
+        text += "\n"
+    return text
+
+def send_message(supplier, categories, ignored_categories, ignored_subcategories, ignored_articles):
+    text = "Hallo,\n\ndie Liste an automatisch ausgelesenen Artikeln von " + supplier + " befindet sich im Anhang.\n"
+    text += "Sie kann unter folgendem Link hochgeladen werden: (Häkchen bei 'Artikel löschen, die nicht in der hochgeladenen Datei sind' setzen!)\n"
+    text += "\n" # Link
+    text += "\n\nAusgelesene Kategorien:\n"
+    text += listCategories(categories)
+    if ignored_categories:
+        text += "\n\nIgnorierte Kategorien:\n"
+        text += listCategories(ignored_categories)
+    if ignored_subcategories:
+        text += "\n\nIgnorierte Unterkategorien:\n"
+        text += listCategories(ignored_subcategories)
+    if ignored_articles:
+        text += "\n\nIgnorierte Artikel:\n"
+        for article in ignored_articles:
+            text += "#" + str(article.order_number) + " " + article.name + " " + article.unit
+            if article.manufacturer:
+                text += " von " + article.manufacturer
+            if article.origin:
+                text += " aus " + article.origin
+    print(text)
 
 def write_csv(supplier, articles):
     rows = get_data_from_articles(articles=articles)
